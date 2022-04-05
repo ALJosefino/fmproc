35106b1897e2ec6890f302884d4ba256b3ddecf5
Andre Josefino <aljosefino@gmail.com>
2022-04-05 11:20:26 -0300

    routes with resoureces and index show data

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "lawsuits#index"
-  get "/lawsuit", to: "lawsuit#index"
+   root "lawsuits#index"
+  # get "/lawsuit", to: "lawsuit#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lawsuit
 end
