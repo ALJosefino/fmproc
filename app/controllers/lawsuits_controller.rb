3bc209b4294e3b0eedf06047d8125e10478c0db8
Andre Luis de Oliveira Josefino <ALJosefino@GMail.com>
2022-04-12 16:29:32 -0300

    simple crud is complete

--- app/controllers/lawsuits_controller.rb
+++ app/controllers/lawsuits_controller.rb
@@ -1,11 +1,12 @@
 class LawsuitsController < ApplicationController
+
+  before_action :set_lawsuit, only: [:show, :edit, :update]
+  
   def index
     @lawsuits = Lawsuit.all
   end
 
-  def show
-    @lawsuit = Lawsuit.find(params[:id])
-  end
+  def show; end
 
   def new
     @lawsuit = Lawsuit.new
@@ -17,28 +18,35 @@ class LawsuitsController < ApplicationController
     if @lawsuit.save
       redirect_to @lawsuit
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
   
-  def edit
-    @lawsuit = Lawsuit.find(params[:id])
-  end
+  def edit; end
   
   def update
-    @lawsuit = Lawsuit.find(params[:id])
-    
     if @lawsuit.update(lawsuit_params)
       redirect_to @lawsuit
     else
       render :edit
     end
-  
+  end
+
+  def destroy
+    @lawsuit = Lawsuit.find(params[:id])
+    @lawsuit.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   private
 
+  def set_lawsuit
+    @lawsuit = Lawsuit.find(params[:id])
+  end
+
   def lawsuit_params
     params.require(:lawsuit).permit(:lawsuit_date, :lawsuit_number, :lawsuit_complainant, :lawsuit_defendant, :lawsuit_case_description)
   end
+  
 end
